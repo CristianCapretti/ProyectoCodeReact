426b633489f795b3662eb77bde52b5b33a23e632
Cristian <cristian.capretti93@gmail.com>
2023-05-11 00:19:28 -0300

    Agrego nav basico

--- proyecto-react/src/components/Header.jsx
+++ proyecto-react/src/components/Header.jsx
@@ -1,9 +1,15 @@
 const Header=()=>{
 
     return(
-        //nav
         <div>
-            <h1>Hola</h1>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                     <a className="navbar-brand" href={"#"}>
+                        <img src={"/docs/5.3/assets/brand/bootstrap-logo.svg"} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+                        Bootstrap
+                    </a>
+                </div>
+            </nav>
         </div>
     )
 

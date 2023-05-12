9295094c3317ec883c767472989f474826ec4abb
Cristian <cristian.capretti93@gmail.com>
2023-05-12 09:01:09 -0300

    Mejoras

--- proyecto-react/src/components/CartWidget.jsx
+++ proyecto-react/src/components/CartWidget.jsx
@@ -4,7 +4,7 @@ const CartWidget=()=>{
     return (
             <button type="button" className="btn btn-light position-relative">
                 <img src={Cart} alt="Carrito" width={20} />
-                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">5</span>
             </button>
             
        
